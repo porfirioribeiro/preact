0e1f7787cc6264957235ee90dcecc3aa365e3334
Porfirio Ribeiro <porfirioribeiro@gmail.com>
2020-01-18 14:41:22 +0000

    Add store initializer

--- composition/src/store.js
+++ composition/src/store.js
@@ -7,17 +7,19 @@ export function readable(value) {
 	return { subscribe: createStore(value).subscribe };
 }
 
-export function createStore(value) {
+export function createStore(value, start = noop) {
 	const subscribers = [];
+	let stop;
 	function get() {
 		return value;
 	}
 	function set(newValue) {
 		if (value != value ? newValue == newValue : value !== newValue) {
 			value = newValue;
-			subscribers.some(f => {
-				f(value);
-			});
+			if (stop)
+				subscribers.some(f => {
+					f(value);
+				});
 		}
 	}
 	function update(fn) {
@@ -25,16 +27,19 @@ export function createStore(value) {
 	}
 	function subscribe(callback) {
 		subscribers.push(callback);
+		// @ts-ignore
+		if (subscribers.length === 1) stop = start(set) || noop;
 		callback(value);
 		return () => {
 			const index = subscribers.indexOf(callback);
 			if (index !== -1) subscribers.splice(index, 1);
+			if (subscribers.length === 0) {
+				stop();
+				stop = null;
+			}
 		};
 	}
 	return { get, set, update, subscribe };
 }
 
-// export function subscribeTo(store, callback) {
-// 	const unsub = store.subscribe(callback);
-// 	return unsub.unsubscribe ? () => unsub.unsubscribe() : unsub;
-// }
+function noop() {}
